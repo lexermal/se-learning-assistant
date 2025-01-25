b5c02c96b446baf118afed567052c3db38b83f3f
lexermal <contact@weixler.me>
2025-01-25 14:18:51 +0100

    fix: Disabling elemenlabs api

--- app/api/speech/Elevenlabs.ts
+++ app/api/speech/Elevenlabs.ts
@@ -1,20 +1,20 @@
-import { env } from "@/utils/constants";
-import { ElevenLabsClient } from "elevenlabs";
+// import { env } from "@/utils/constants";
+// import { ElevenLabsClient } from "elevenlabs";
 
 export async function elevenlabsTTS(input: string, voiceId: string, language: string) {
-    const elevenlabs = new ElevenLabsClient({ apiKey: env.ELEVENLABS_API_KEY });
-    try {
-        const audio = await elevenlabs.generate({
-            text: input,
-            voice: voiceId,
-            language_code: language,
-            model_id: "eleven_flash_v2_5",
-        });
+    // const elevenlabs = new ElevenLabsClient({ apiKey: env.ELEVENLABS_API_KEY });
+    // try {
+    //     const audio = await elevenlabs.generate({
+    //         text: input,
+    //         voice: voiceId,
+    //         language_code: language,
+    //         model_id: "eleven_flash_v2_5",
+    //     });
 
-        return new Response(audio as any, { headers: { "Content-Type": "audio/mpeg" } });
-    } catch (error: any) {
-        const textBody = await error.body.text();
-        console.error('Error at generating elevenlabs voice:', textBody);
-        return Response.json(error, { status: error.statusCode });
-    }
+    //     return new Response(audio as any, { headers: { "Content-Type": "audio/mpeg" } });
+    // } catch (error: any) {
+    //     const textBody = await error.body.text();
+    //     console.error('Error at generating elevenlabs voice:', textBody);
+    //     return Response.json(error, { status: error.statusCode });
+    // }
 }
